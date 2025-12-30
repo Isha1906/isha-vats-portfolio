4697bb30733ce05994c713fe7cc6e6912760d989
Isha Vats <ivats.nic@gmail.com>
2025-12-30 19:58:27 +0530

    Add contact me form

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,8 +1,8 @@
 const Contact = () => {
   return (
-    <section id="contact">
-      <h2>Contact</h2>
-      <div className="contact-card">
+    <section id="contact" className="container py-5">
+      <h2>Get In Touch</h2>
+      <div className="contact-card row mb-5 text-center">
       <p>Email:{" "}
       <a href="mailto:ivats.nic@gmail.com">
         ivats.nic@gmailcom
@@ -28,6 +28,59 @@ const Contact = () => {
         </a>
       </p>
       </div> 
+
+      <div className="row justify-content-center">
+        <div className="col-md-8 col-lg-6">
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            netlify-honeypot="bot-field"
+          >
+            
+            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="bot-field" />
+
+            <div className="mb-3">
+              <label className="form-label">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                placeholder="Your Name"
+                required
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="Your Email"
+                required
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label">Message</label>
+              <textarea
+                name="message"
+                className="form-control"
+                rows="4"
+                placeholder="Your Message"
+                required
+              ></textarea>
+            </div>
+
+            <button type="submit" className="btn btn-primary w-100">
+              Send Message
+            </button>
+          </form>
+        </div>
+      </div>
+      
     </section>
   );
 };
